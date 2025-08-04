d1cc251d248c4e8c62d0dffb9b375932ecc0fe5f
Hetarou <isida0427@gmail.com>
2025-08-04 23:51:08 +0900

    TitleSceneとMainScenarioSceneの追加、そしてcsv読み込みの実験

---
+++ Nosferato/Assets/HetarouFolder/SentenceReader.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class SentenceReader : MonoBehaviour
+{
+    private TextAsset csvFile; // CSVファイル
+    private List<string[]> csvData = new List<string[]>(); // CSVファイルの中身を入れるリスト
+
+    void Start()
+    {
+        csvFile = Resources.Load("Sample") as TextAsset; // ResourcesにあるCSVファイルを格納
+        StringReader reader = new StringReader(csvFile.text); // TextAssetをStringReaderに変換
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine(); // 1行ずつ読み込む
+            csvData.Add(line.Split(',')); // csvDataリストに追加する
+        }
+
+        for (int i = 0; i < csvData.Count; i++) // csvDataリストの条件を満たす値の数（全て）
+        {
+            // データの表示
+            if (csvData[i][0] != null) Debug.Log("日本語：" + csvData[i][0] + ", English：" + csvData[i][1]);
+            else Debug.Log("nullです");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
