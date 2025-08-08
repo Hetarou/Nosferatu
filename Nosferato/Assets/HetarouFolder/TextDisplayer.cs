99783042afc1166b932642a16293f24ddea99f00
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2025-08-09 02:42:04 +0900

    PublicStaticStatusつくった TextDisplayerみたいにPublicStaticStatusいじるべし わたしはそこからセーブロードする

---
+++ Nosferato/Assets/HetarouFolder/TextDisplayer.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class TextDisplayer : MonoBehaviour//PublicStaticStatusを更新することもする //SaveSceneはPublicStaticStatusを参照してセーブする
+{
+    private TextAsset csvFile; // CSVファイル
+    private List<string[]> csvData = new List<string[]>(); // CSVファイルの中身を入れるリスト
+
+    public TMP_Text messageText;            // 文章のText
+    public TMP_Text nameText;               //名前のText
+    public float charDelay = 0.05f;         // 文字送りの速さ
+
+    public int messageIndex;
+
+    int RowNumber = 0;
+
+    string threadNumber;
+    string LastThreadNumber;
+
+    private bool isTyping = false;
+
+    void Start()
+    {
+        csvFile = Resources.Load("Sample") as TextAsset;        // ResourcesにあるCSVファイルを格納
+        StringReader reader = new StringReader(csvFile.text);   // TextAssetをStringReaderに変換
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine(); // 1行ずつ読み込む
+            csvData.Add(line.Split(',')); // csvDataリストに追加する
+        }
+
+        threadNumber = csvData[0][0];
+        LastThreadNumber = threadNumber;
+
+
+        StartCoroutine(DisplayChar(csvData[0][1]));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            RowNumber++;
+            threadNumber = csvData[RowNumber][0];
+
+            if (threadNumber != LastThreadNumber)
+            {
+                messageText.text = "";
+                LastThreadNumber = threadNumber;
+            }
+            StartCoroutine(DisplayChar(csvData[RowNumber][1]));
+        }
+    }
+
+    IEnumerator DisplayChar(string message)
+    {
+        isTyping = true;
+
+        foreach (char c in message)
+        {
+            messageText.text += c;
+            yield return new WaitForSeconds(charDelay);
+        }
+        messageText.text += "\n";
+        PublicStaticStatus.ReferencedRowToSave = RowNumber;
+        PublicStaticStatus.DisplayedText= messageText.text;
+        isTyping = false;
+    }
+}
---
+++ Nosferato/Assets/Script/PublicStaticStatus.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PublicStaticStatus : MonoBehaviour
+{
+    public static int ReferencedRowToSave;
+    public static string DisplayedText;
+    //public static bool IsCleared;
+    //public static string ConfigSetting;
+}
