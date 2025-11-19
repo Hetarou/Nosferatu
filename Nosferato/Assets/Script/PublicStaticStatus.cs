dfa5ac0ce71d2d7286ea71f0fbd97f67515f8004
NanamuneCreate0 <nanamuneCreate0@gmail.com>
2025-11-19 23:16:29 +0900

    PublicStaticStatus解説付ける

--- Nosferato/Assets/Script/PublicStaticStatus.cs
+++ Nosferato/Assets/Script/PublicStaticStatus.cs
@@ -5,12 +5,11 @@ public class PublicStaticStatus : MonoBehaviour
     public static int RowToSave;//セーブ対象
     public static string DisplayedText;//いらなければ使わないけどどう？
 
-    public static int ScreenMode;
-    public static int Font;//"RanovePop","KakuGothic","Minchoutai"
-    public static int ReadingSpeed;
-    public static int Volume;
-    public static int BGMVolume;
-    public static int SEVolume;
-
-    public static string LastScene;
+    public static int ScreenMode;//0:ウィンドウ,1:フルスクリーン,2:疑似フルスクリーン
+    public static int Font;//0:ラノベポップ,1:角ゴシック,2:明朝体
+    public static int ReadingSpeed;//0:普通,1:速い,2:瞬時
+    public static int Volume;//0-10
+    public static int BGMVolume;//0-10
+    public static int SEVolume;//0-10
+    public static bool IsCleared;
 }
